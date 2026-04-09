a89b8aa81e543fb5f8abfa3b9e25e6d7154b0d9b
roquim <roquimgabriel@hotmail.com>
2026-04-08 21:52:51 -0300

    add loginPage|

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,10 +1,15 @@
 import { test } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+
+let loginPage
+
+test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page)
+})
 
 test('deve logar como administrador', async ({ page }) => {
+    await loginPage.visit()
 
-    await page.goto('http://localhost:3000/login')
-
-    const loginForm = page.locator('.login-form')
-    await expect(loginForm).toBeVisible()
+    await loginPage.submit('tests@ese.com.br', '123456')
 
 })
